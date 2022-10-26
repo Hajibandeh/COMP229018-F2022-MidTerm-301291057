db186632d734dcbf4570f6282b9d9c6e1121727f
Hajibandeh <113070569+Hajibandeh@users.noreply.github.com>
2022-10-26 18:10:04 -0400

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// Do not expose your credentials in your code.
+let atlasDB="mongodb+srv://Hajibandeh:a9QCe0F97fF7lVmm@cluster018.qxx0hxo.mongodb.net/todo?retryWrites=true&w=majority"
+
+
+
+// Database setup
+let mongoose = require('mongoose');
+
+module.exports = function(){
+
+    mongoose.connect(atlasDB);
+    let mongodb = mongoose.connection;
+
+    mongodb.on('error', console.error.bind(console, 'Connection Error:'));
+    mongodb.once('open', ()=>{
+        console.log('===> Connected to MongoDB.');
+    })
+
+    return mongodb;
+}
